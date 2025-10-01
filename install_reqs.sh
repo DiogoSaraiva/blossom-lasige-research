d01314ee55ee686b6572e95a42ad8a09c197c610
Diogo Saraiva <diogosaraiva.prt@gmail.com>
2025-10-01 12:02:08 +0100

    Added new dependency

--- install_reqs.sh
+++ install_reqs.sh
@@ -2,4 +2,4 @@ sudo apt update
 
 curl https://pyenv.run | bash
 
-sudo apt install -y ffmpeg totem libportaudio2 portaudio19-dev libportaudiocpp0
+sudo apt install -y ffmpeg totem libportaudio2 portaudio19-dev libportaudiocpp0 libxcb-cursor0
